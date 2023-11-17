e27e5518eb074ea23fc59051e0800155e5c182b8
Anup <anupsingh841208@gmail.com>
2023-11-17 14:51:29 +0530

    basic template working fine

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import { NavLink, Link } from "react-router-dom";
+import { FiGithub } from "react-icons/fi";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p data-testid="footer-logo" className="paragraph colwhite">
+       Tasty Treats
+      </p>
+      <p className="paragraph colwhite">
+        Developed by Team Geeky Nerdz{" "}
+        <a href="https://github.com/anups1ngh">
+          {" "}
+          <FiGithub color="white" size="1rem" to="/about" />{" "}
+        </a>
+      </p>
+      <ul>
+        <NavLink to="/">
+          <li>Home</li>
+        </NavLink>
+        <NavLink to="/about">
+          <li>About</li>
+        </NavLink>
+        <NavLink to="/contact">
+          <li>Contact</li>
+        </NavLink>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
